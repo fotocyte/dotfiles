8e7b7760b148dd01c57d9c5c21cd17ab62fe7562
fotocyte <me@dontalton.com>
2021-07-11 15:17:56 -0700

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -17,7 +17,7 @@ echo 'Server = http://mirror.arizona.edu/archlinux/$repo/os/$arch' > /etc/pacman
 wpa_supplicant -B -i wlan0 -c<(wpa_passphrase WIRELESSNET PASSWORD)
 
 pacman -Sy
-pacstrap /mnt base linux linux-firmware intel-ucode grub efibootmgr util-linux networkmanager networkmanager-openvpn
+pacstrap /mnt base linux linux-firmware grub efibootmgr util-linux networkmanager networkmanager-openvpn
 
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Phoenix /etc/localtime
@@ -38,6 +38,8 @@ EOF
 
 arch-chroot /mnt
 mount /dev/sda1 /mnt
+# install either amd-ucode or intel-ucode
+# pacman -S *-ucode
 grub-install --target=x86_64-efi --efi-directory=/mnt --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
